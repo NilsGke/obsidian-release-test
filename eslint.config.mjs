e0d076e0e5c7769955379991fbd25e9c7c4e9512
NilsGke <n.j.goeke@gmail.com>
2026-05-29 21:00:30 +0200

    setup

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import obsidianmd from "eslint-plugin-obsidianmd";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig(
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: ["eslint.config.js", "manifest.json"],
+                },
+                tsconfigRootDir: import.meta.dirname,
+                extraFileExtensions: [".json"],
+            },
+        },
+    },
+    ...obsidianmd.configs.recommended,
+    globalIgnores([
+        "node_modules",
+        "dist",
+        "esbuild.config.mjs",
+        "eslint.config.js",
+        "version-bump.mjs",
+        "versions.json",
+        "main.js",
+    ]),
+);
